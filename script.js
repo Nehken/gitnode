72d2a22cd8986fcf068372a295cae18345739bbb
Gonzalo Lifrieri <gonza.lifrieri@gmail.com>
2020-02-18 20:39:39 -0300

    agrego script

---
+++ script.js
@@ -0,0 +1,12 @@
+const readline = require("readline")
+const prettier = require("prettier")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+rl.question("Insertar una ruta de archivo", (answer) => {
+    
+    
+})
