9ef24dbbb54e5f79bd60360293b25356a08ceb5d
Lucas <lukymassaro@gmail.com>
2020-02-18 22:01:14 -0300

    WIP

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const readline = require("readline");
-const prettier = require("prettier");
 const fs = require("fs");
+const js_bf = require("js-beautify");
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -8,11 +8,27 @@ const rl = readline.createInterface({
   });
 
 rl.question("Insertar una ruta de archivo: ", (answer) => {
-  let ultIndice = fs.readFile(answer, (err, file) => {    //readdir lee la ruta de un archivo
+  let split = answer.split('.');
+  let extension = split[split.length-1];
+  let archivo = fs.readFile(answer, (err, file) => {    //readdir lee la ruta de un archivo
     if(err){
       console.log(err);
       return;
     }
-    console.log(file.toString());
+    return file;
   });
-})
+  switch (extension){
+    case 'html':
+      console.log('holis');
+      break;
+    case 'css':
+      console.log('holis');
+      break;
+    case 'js':
+      js_bf(archivo, {indent_size: 2, space_in_empty_paren: true});
+      break;
+    default:
+      console.log('default');
+  }
+  
+  });
